e0ca440fd52a7a649395bad4acff245065611b01
Michal <michal.pyszynski93@gmail.com>
2019-05-12 13:31:24 +0200

    Updates

---
+++ precache-manifest.e7eee7fd46f52738744519983479b505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0510b352cf90b167d2b1e5b31fac4c47",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "f8fbe51d52e5d91cfcb6",
+    "url": "/store/static/css/main.3e9729a4.chunk.css"
+  },
+  {
+    "revision": "6868422315f0a53374a3",
+    "url": "/store/static/js/2.d6935195.chunk.js"
+  },
+  {
+    "revision": "f8fbe51d52e5d91cfcb6",
+    "url": "/store/static/js/main.37a8656b.chunk.js"
+  },
+  {
+    "revision": "4642218ef387c551e27b",
+    "url": "/store/static/js/runtime~main.852bf9be.js"
+  },
+  {
+    "revision": "93a7fd8bea85d65317e08ca2da0dd552",
+    "url": "/store/static/media/PogChamp.93a7fd8b.png"
+  },
+  {
+    "revision": "1f11e8f01a7599e14765aee025827703",
+    "url": "/store/static/media/pepe.1f11e8f0.png"
+  }
+]);
